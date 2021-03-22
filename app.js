128ecd5cd52b8ce5eaee79f9fc83a58b51ca8a02
Kinraec3981 <70333150+Kinraec3981@users.noreply.github.com>
2021-03-22 21:32:38 +0100

    Update app.js

--- app.js
+++ app.js
@@ -2,10 +2,13 @@ function myfunction(){
 	document.getElementById("first").style.display = "none";
 	document.getElementById("second").style.display = "none";
 	document.getElementById("third").style.display = "none";
+	document.getElementById("fourth").style.display = "none";
 	var first = document.getElementById("first").value;
 	document.getElementById("day").innerHTML = ("Day: " + first);
 	var second = document.getElementById("second").value;
 	document.getElementById("meters").innerHTML = ("Meters Run: " + second + " meters");
+	var fourth = document.getElementById("fourth").value;
+	document.getElementById("success").innerHTML = (fourth);
 	document.getElementById("page").style.background = "#add8e6";
 	document.getElementById("success").style.display = "block";
 
